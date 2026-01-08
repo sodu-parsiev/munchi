99cb9449e9a24d77f2945dc41ea08f0ccad8a264
sodu <sodiq915@gmail.com>
2026-01-09 00:24:47 +0500

    install sanctum

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\RedemptionController;
 use App\Http\Controllers\RewardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/me', [MeController::class, 'show'])->name('me.show');
+Route::middleware('auth:sanctum')->get('/me', [MeController::class, 'show'])->name('me.show');
 Route::post('/postback', [PostbackController::class, 'handle'])->name('postback.handle');
 Route::post('/redemptions', [RedemptionController::class, 'store'])->name('redemptions.store');
 Route::get('/rewards', [RewardController::class, 'index'])->name('rewards.index');
